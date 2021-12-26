a6791243925225ad900b0d466e07cc9693831179
Francesca <francesca.fornasier@icloud.com>
2021-12-26 23:43:12 +0100

    nuovo file

--- lab01es01.m
+++ lab01es01.m
@@ -12,7 +12,23 @@ es=menu('Simulazione della riaposta del sistema meccanico',...
         '2.c) beta=10,   K=20, x0=[0;0],   F(t)=cos(4t);',...
         '2.d) beta=0.1,  K=2,  x0=[0;0.2], F(t)=cos(4t)' );
 
-% comando ss crea uno state-space model, un oggetto che contiene la
+switch es
+        case 1, beta=0.1;  k=2;  x0=[0;0];   w0=0; tmax=20;
+        case 2, beta=0.01; k=2;  x0=[0;0];   w0=0; tmax=200;
+        case 3, beta=10;   k=20; x0=[0;0];   w0=0; tmax=10;
+        case 4, beta=0.1;  k=2;  x0=[0;0.2]; w0=0; tmax=20;
+        case 5, beta=0.1;  k=2;  x0=[0;0];   w0=4; tmax=10;
+        case 6, beta=0.01; k=2;  x0=[0;0];   w0=4; tmax=10;
+        case 7, beta=10;   k=20; x0=[0;0];   w0=4; tmax=10;
+        case 8, beta=0.1;  k=2;  x0=[0;0.2]; w0=4; tmax=10;
+end
+
+m = 0.2;
+f0 = 1;
+
+tmax = 20;
+
+% Comando ss crea uno state-space model, un oggetto che contiene la
 % rappresentazione in variabili di stato del sistema.
 % SYS = ss(A,B,C,D);
 
@@ -22,74 +38,33 @@ es=menu('Simulazione della riaposta del sistema meccanico',...
 % Es:  t=0:0.01:5;  u=sin(t);
 % lsim in questa forma plotta la risposta, non ci restituisce nulla.
 % Per far si' che ci restituisca qualcosa va usato nella forma:
-% [Y,T,X]=lsim(SYS,U,T,X0);    
+% [Y,T,X]=lsim(SYS,U,T,X0);
 % X e' il vettore dell'evoluzione delle variabili di stato
 
-
 %          ___     B   A    K    |//
-%   F <---| M |---·]---•---NNN---|//
-%          ---                   |//
-%  <-----------------------------|//
-
-% u = [F];   x = [x1; x2] = [v; pa];   y = [v]
+%   F <---| M |---·]---•---NNN---|//            u = [F];
+%          ---                   |//            x = [x1; x2] = [v; pa];
+%  <-----------------------------|//            y = [v]
+%
 % Rappresentazione in variabili di stato:
 %    x1' = -K/M * x2 + 1/M * u
 %    x2' = x1 - K/B * x2
 %    y = x1
 
-M = 0.2;
-F0 = 1;
-w0 = 0;
+t=0:0.01:tmax;
+u = f0*cos(w0*t);
 
-t=0:0.01:10;
+A = [0,-k/m;1,-k/beta];
+B = [1/m;0];
+C = [1,0];
+D = 0;
 
-F = F0*cos(w0*t);
-for es = 1:8
-    pause
-    switch es
-        case 1, beta=0.1;  k=2;  x0=[0;0];   w0=0; tmax=20;
-        case 2, beta=0.01; k=2;  x0=[0;0];   w0=0; tmax=200;
-        case 3, beta=10;   k=20; x0=[0;0];   w0=0; tmax=10;
-        case 4, beta=0.1;  k=2;  x0=[0;0.2]; w0=0; tmax=20;
-        case 5, beta=0.1;  k=2;  x0=[0;0];   w0=4; tmax=10;
-        case 6, beta=0.01; k=2;  x0=[0;0];   w0=4; tmax=10;
-        case 7, beta=10;   k=20; x0=[0;0];   w0=4; tmax=10;
-        case 8, beta=0.1;  k=2;  x0=[0;0.2]; w0=4; tmax=10;
-    end
-    
-    A = [0,-k/M;1,-k/beta];
-    B = [1/M;0];
-    C = [1,0];
-    D = [0];
-    
-    system = ss(A, B, C, D);
-    
-    figure,lsim(system,F,t,x0)
-end
+% calcolo numerico dell'evoluzione di stati e uscita del sistema dinamico
+system = ss(A, B, C, D);
+[y,tsim,x] = lsim(system,u,t,x0)
 
-w0 = 4
-F2 = F0*cos(w0*t);
-for es = 1:8
-    pause
-    switch es
-        case 1, beta=0.1;  k=2;  x0=[0;0];   w0=0; tmax=20;
-        case 2, beta=0.01; k=2;  x0=[0;0];   w0=0; tmax=200;
-        case 3, beta=10;   k=20; x0=[0;0];   w0=0; tmax=10;
-        case 4, beta=0.1;  k=2;  x0=[0;0.2]; w0=0; tmax=20;
-        case 5, beta=0.1;  k=2;  x0=[0;0];   w0=4; tmax=10;
-        case 6, beta=0.01; k=2;  x0=[0;0];   w0=4; tmax=10;
-        case 7, beta=10;   k=20; x0=[0;0];   w0=4; tmax=10;
-        case 8, beta=0.1;  k=2;  x0=[0;0.2]; w0=4; tmax=10;
-    end
-    
-    A = [0,-k/M;1,-k/beta];
-    B = [1;0];
-    C = [1,0];
-    D = [0];
-    
-    system2 = ss(A, B, C, D);
-    
-    figure,lsim(system2,F2,t,x0)
-end
+% confronto grafico dei risultati ottenuti
 
-
+figure(1), plot(tsim, x(:,1)), grid on, zoom on, title('Evoluzione dello stato x_1'), xlabel('tempo (in s)'), ylabel('velocità v (in m/s)')
+figure(2), plot(tsim, x(:,2)), grid on, zoom on, title('Evoluzione dello stato x_2'), xlabel('tempo (in s)'), ylabel('posizione p_A (in m)')
+figure(3), plot(tsim, y), grid on, zoom on, title('Evoluzione dell''uscita y'), xlabel('tempo (in s)'), ylabel('velocità v (in m/s)')
--- lab01es02.m
+++ lab01es02.m
@@ -2,26 +2,66 @@ clear all
 close all
 clc
 
-c = 0.2;
-w0=0;
-i0=1;
-t=0:0.01:10;
-i = i0*cos(w0*t);
+es=menu('Simulazione della riaposta del sistema meccanico',...
+        '1.a) beta=0.1, K=2,  x0=[0;0],   F(t)=1;',...
+        '1.b) beta=0.01, K=2,  x0=[0;0],   F(t)=1;',...
+        '1.c) beta=10,   K=20, x0=[0;0],   F(t)=1;',...
+        '1.d) beta=0.1,  K=2,  x0=[0;0.2], F(t)=1;',...
+        '2.a) beta=0.1,  K=2,  x0=[0;0],   F(t)=cos(4t);',...
+        '2.b) beta=0.01, K=2,  x0=[0;0],   F(t)=cos(4t);',...
+        '2.c) beta=10,   K=20, x0=[0;0],   F(t)=cos(4t);',...
+        '2.d) beta=0.1,  K=2,  x0=[0;0.2], F(t)=cos(4t)' );
 
-for es = 1:8
-   switch es
-       case 1, R=10; L=0.5; x0=[0;0];
-       case 2, R=100; L=0.5; x0=[0;0];
-       case 3, R=0.1; L=0.05; x0=[0;0];
-       case 4, R=10; L=0.5; x0=[0;0.2];
-   end
-   
-   A = [0, -1/c; 1/L, 0];
-   B = [1/c; 0];
-   C = [1, 0];
-   D = [0];
-   
-   SYS = ss(A, B, C, D);
-   figure,lsim(SYS,i,t,x0)
-   
+switch es
+        case 1, beta=0.1;  k=2;  x0=[0;0];   w0=0; tmax=20;
+        case 2, beta=0.01; k=2;  x0=[0;0];   w0=0; tmax=200;
+        case 3, beta=10;   k=20; x0=[0;0];   w0=0; tmax=10;
+        case 4, beta=0.1;  k=2;  x0=[0;0.2]; w0=0; tmax=20;
+        case 5, beta=0.1;  k=2;  x0=[0;0];   w0=4; tmax=10;
+        case 6, beta=0.01; k=2;  x0=[0;0];   w0=4; tmax=10;
+        case 7, beta=10;   k=20; x0=[0;0];   w0=4; tmax=10;
+        case 8, beta=0.1;  k=2;  x0=[0;0.2]; w0=4; tmax=10;
 end
+
+m = 0.2;
+f0 = 1;
+
+tmax = 20;
+
+t=0:0.01:tmax;
+u = f0*cos(w0*t);
+
+A = [0,-k/m;1,-k/beta];
+B = [1/m;0];
+C = [1,0];
+D = 0;
+
+% Stesso sistema di prima, calcolo le funzioni di trasferimento 
+
+[num,den] = ss2tf(A,B,C,D)
+
+%% ESERCIZIO 3
+
+% Calcolare analiticamente, per condizioni iniziali nulle, le risposte dei
+% sistemi dinamici ai seguenti ingressi:
+%  -  gradino di ampiezza 1
+%  -  rampa unitaria
+%  -  u0*cos(4t)
+
+ingresso = menu('tipo di ingresso del sistema','u(t) = u0','u(t) = t','u(t) = u0*cos(4*t)');
+
+s=tf('s');
+
+switch ingresso
+    case 1, U = 1/s;
+    case 2, U = 1/s^2;
+    case 3, U = s/(s^2+4^2);
+end
+
+H = tf(num,den)
+
+Y = H * U;
+
+[numY,denY] = tfdata(Y,'v')
+
+[residui,poli,K] = residue(numY,denY)
